30562381268f2fd672a8081fa2ef2756d6215047
Juan Pascale <juan.pascale@sabf.org.ar>
2016-09-28 02:04:12 +0200

    Queue add funca

--- logger.erl
+++ logger.erl
@@ -1,5 +1,5 @@
 -module(logger).
--export([start/1, stop/1]).
+-export([start/1, stop/1, queue_add/4]).
 
 start(Nodes) ->
 	spawn_link(fun() ->init(Nodes) end).
@@ -8,16 +8,20 @@ stop(Logger) ->
 	Logger ! stop.
 
 init(Nodes) ->
-	Clock = time:clock(Nodes).
-	Queue = 
-	loop().
+	Clock = time:clock(Nodes),
 
-loop() ->
+	% Create an empty hold-back queue.
+	Queue = lists:foldr(fun(Elem, List) -> [ {Elem, []} | List] end, [], Nodes),
+	loop(Queue, Clock).
+
+loop(Queue, Clock) ->
 	receive
 
 		{log, From, Time, Msg} ->
-			log(From, Time, Msg),
-			loop();
+			%log(From, Time, Msg),
+			NewClock = time:update(From, Time, Clock),
+			NewQueue = queue_add(From, Time, Msg, Queue),
+			loop(NewQueue, NewClock);
 
 		stop ->
 			ok
@@ -25,3 +29,8 @@ loop() ->
 
 log(From, Time, Msg) ->
 	io:format("log: ~w ~w ~p~n", [Time, From, Msg]).
+
+queue_add(Name, Time, Msg, Queue) ->
+	{_, NodeList} = lists:keyfind(Name, 1, Queue),
+	NewNodeList = [{Time, Msg} | NodeList],
+	lists:keyreplace(Name, 1, Queue, {Name, NewNodeList}).
