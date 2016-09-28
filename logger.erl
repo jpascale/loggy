9d56384c19e5a77242105bb3df627759c41fc6a8
Juan Pascale <juan.pascale@sabf.org.ar>
2016-09-28 03:52:26 +0200

    log and queue add working

--- logger.erl
+++ logger.erl
@@ -1,5 +1,5 @@
 -module(logger).
--export([start/1, stop/1, queue_add/4]).
+-export([start/1, stop/1, queue_add/4, log/2]).
 
 start(Nodes) ->
 	spawn_link(fun() ->init(Nodes) end).
@@ -11,26 +11,46 @@ init(Nodes) ->
 	Clock = time:clock(Nodes),
 
 	% Create an empty hold-back queue.
-	Queue = lists:foldr(fun(Elem, List) -> [ {Elem, []} | List] end, [], Nodes),
+	%Queue = lists:foldr(fun(Elem, List) -> [ {Elem, []} | List] end, [], Nodes),
+	Queue = [],
 	loop(Queue, Clock).
 
 loop(Queue, Clock) ->
 	receive
-
 		{log, From, Time, Msg} ->
-			%log(From, Time, Msg),
 			NewClock = time:update(From, Time, Clock),
 			NewQueue = queue_add(From, Time, Msg, Queue),
-			loop(NewQueue, NewClock);
+			NextQueue = log(NewQueue, NewClock),
+			loop(NextQueue, NewClock);
 
 		stop ->
 			ok
 	end.
 
-log(From, Time, Msg) ->
+log_print(From, Time, Msg) ->
 	io:format("log: ~w ~w ~p~n", [Time, From, Msg]).
 
-queue_add(Name, Time, Msg, Queue) ->
-	{_, NodeList} = lists:keyfind(Name, 1, Queue),
-	NewNodeList = [{Time, Msg} | NodeList],
-	lists:keyreplace(Name, 1, Queue, {Name, NewNodeList}).
+queue_add(Name, Time, Msg, []) -> [{Name, Time, Msg}];
+queue_add(Name, Time, Msg, Queue) -> 
+	[First | Rest] = Queue,
+	{_, T, _} = First,
+	if (T >= Time) -> [{Name, Time, Msg} | Queue];
+		true -> [First | queue_add(Name, Time, Msg, Rest)]
+	end.
+	%[{Name, Time, Msg} | Queue].
+	%{_, NodeList} = lists:keyfind(Name, 1, Queue),
+	%NewNodeList = [{Time, Msg} | NodeList],
+	%lists:keyreplace(Name, 1, Queue, {Name, NewNodeList}).
+
+log(Queue, Clock) ->
+	lists:foldr(
+			fun(Elem, List) ->
+				{Name, Time, Msg} = Elem,
+				Safe = time:safe(Time, Clock),
+				if (Safe) -> 
+						log_print(Name, Time, Msg),
+						List;
+					true -> [Elem | List]
+				end
+			end,
+			[], Queue).
